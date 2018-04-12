07fb707395790db1e249fbb77b282f1ab28db56c
BokzBCheung <BokzBCheung@gmail.com>
2018-04-12 09:12:35 +0800

    first add gui project
    Signed-off-by: BokzBCheung <BokzBCheung@gmail.com>

---
+++ GUIDemo/guiTkinter.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018-04-12 09:11
+# @Author  : BokzBCheung
+# @Site    : 
+# @File    : guiTkinter.py
+# @Software: PyCharm
+# @license : Copyright(C),BokzBCheung
+# @Contact : BokzBCheung@gmail.com
