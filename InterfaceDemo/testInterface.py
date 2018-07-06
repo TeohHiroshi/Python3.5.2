f44efd759f5670eb34b565856c0410bc9ac50fa5
BokzBCheung <BokzBCheung@gmail.com>
2018-07-06 14:28:15 +0800

    add the interface project
    Signed-off-by: BokzBCheung <BokzBCheung@gmail.com>

---
+++ InterfaceDemo/testInterface.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018-07-06 14:24
+# @Author  : BokzBCheung
+# @Site    : www.github.com/bokzbcheung
+# @File    : testInterface.py
+# @Software: PyCharm
+# @license : Copyright(C),BokzBCheung
+# @Contact : BokzBCheung@gmail.com
+
