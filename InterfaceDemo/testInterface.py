2084454502a1e7a065fea33710228e6ce099522c
BokzBCheung <BokzBCheung@gmail.com>
2018-07-06 17:19:10 +0800

    update the project file of InterfaceTest
    Signed-off-by: BokzBCheung <BokzBCheung@gmail.com>

--- InterfaceDemo/testInterface.py
+++ InterfaceDemo/testInterface.py
@@ -8,3 +8,31 @@
 # @license : Copyright(C),BokzBCheung
 # @Contact : BokzBCheung@gmail.com
 
+from flask import Flask, request,json,jsonify
+succJson = {"test": "成功！"}
+failJson = {"test": "失败！"}
+
+app = Flask(__name__)
+
+@app.route('/testInterface',methods=['POST'])
+def api():
+    if request.method == "POST":
+
+        data = request.get_data()
+        data = json.loads(data)
+
+        print("接受到数据为：",data)
+
+        if data["data"] == "测试数据":
+
+            print("返回数据为：",succJson)
+            return jsonify(succJson)
+
+        else:
+
+            print("返回数据为：",failJson)
+            return jsonify(failJson)
+
+if __name__ == '__main__':
+    app.config['JSON_AS_ASCII'] = False
+    app.run(host='127.0.0.1',port='8080')
