0ab12291b2f6aeb2417a623bdcdd3fd5a16a2400
BokzBCheung <BokzBCheung@gmail.com>
2019-03-06 14:31:53 +0800

    add the fibonacci code
    Signed-off-by: BokzBCheung <BokzBCheung@gmail.com>

--- InterviewTopic/basicTopics.py
+++ InterviewTopic/basicTopics.py
@@ -51,3 +51,14 @@ def sort_Bubble(arrlist):
 arrlist=[4,3,2,5,7,9,0]
 print(sort_Tool(arrlist))
 print(sort_Bubble(arrlist))
+
+# 斐波那契数列
+def fibonacci(n):
+    if n==0:
+        return 0
+    elif n==1:
+        return 1
+    else:
+        return fibonacci(n-1)+fibonacci(n-2)
+
+print(fibonacci(7))
